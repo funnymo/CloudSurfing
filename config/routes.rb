0333d1dff76c648136a88a10999b44f71054acb2
Morgan <mky.wee@gmail.com>
2016-05-19 03:01:11 +0800

    added user's trips/reservations and showing

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
   
+  get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
+  
 end

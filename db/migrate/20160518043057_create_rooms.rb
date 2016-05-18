073680564e087483878d24f089095c2a42eb5ba2
Morgan <mky.wee@gmail.com>
2016-05-18 12:34:27 +0800

    created room model

---
+++ db/migrate/20160518043057_create_rooms.rb
@@ -0,0 +1,27 @@
+class CreateRooms < ActiveRecord::Migration
+  def change
+    create_table :rooms do |t|
+      t.string :home_type
+      t.string :room_type
+      t.integer :accommodate
+      t.integer :bed_room
+      t.integer :bath_room
+      t.string :listing_name
+      t.text :summary
+      t.string :address
+      t.boolean :is_tv
+      t.boolean :is_kitchen
+      t.boolean :is_smoking
+      t.boolean :is_air
+      t.boolean :is_heating
+      t.boolean :is_internet
+      t.boolean :is_gym
+      t.boolean :is_pool
+      t.integer :price
+      t.boolean :active
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517175747) do
+ActiveRecord::Schema.define(version: 20160518043057) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "home_type"
+    t.string   "room_type"
+    t.integer  "accommodate"
+    t.integer  "bed_room"
+    t.integer  "bath_room"
+    t.string   "listing_name"
+    t.text     "summary"
+    t.string   "address"
+    t.boolean  "is_tv"
+    t.boolean  "is_kitchen"
+    t.boolean  "is_smoking"
+    t.boolean  "is_air"
+    t.boolean  "is_heating"
+    t.boolean  "is_internet"
+    t.boolean  "is_gym"
+    t.boolean  "is_pool"
+    t.integer  "price"
+    t.boolean  "active"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "rooms", ["user_id"], name: "index_rooms_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

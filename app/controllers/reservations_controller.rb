ef85e9c9831cb2c4c5bdd7d37ccf4566e4f511d3
Morgan <mky.wee@gmail.com>
2016-05-19 17:33:39 +0800

    added ngrok and paypal

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,51 +1,81 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, except: [:notify]
+
   def preload
     room = Room.find(params[:room_id])
     today = Date.today
     reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
-    
+
     render json: reservations
   end
-  
+
   def preview
     start_date = Date.parse(params[:start_date])
     end_date = Date.parse(params[:end_date])
-    
+
     output = {
       conflict: is_conflict(start_date, end_date)
       }
-    
+
     render json: output
   end
-  
+
   def create
     room = Room.find(params[:room_id])
     if current_user == room.user
       redirect_to room, notice: "You can't book your own room!"
     else
       @reservation = current_user.reservations.create(reservation_params)
-      redirect_to @reservation.room, notice: "Your reservation has been created.."
+      if @reservation
+        values = {
+          business: 'mky.wee-facilitator@gmail.com',
+          cmd: '_xclick',
+          upload: 1,
+          notify_url: 'http://cb48685c.ngrok.io/notify',
+          amount: @reservation.total,
+          item_name: @reservation.room.listing_name,
+          item_number: @reservation.id,
+          quantity: '1',
+          return: 'http://cb48685c.ngrok.io/your_trips'
+        }
+
+        redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+      else
+        redirect_to @reservation.room, alert: "Your reservation can't be made.."
+      end
     end
   end
-  
-  def your_trips
-    @trips = current_user.reservations
+
+  protect_from_forgery except: [:notify]
+  def notify
+    params.permit!
+    status = params[:payment_status]
+    reservation = Reservation.find(params[:item_number])
+    if status == "Completed"
+      reservation.update_attributes status: true
+    else
+      reservation.destroy
+    end
+    render nothing: true
   end
-  
+
+  protect_from_forgery except: [:your_trips]
+  def your_trips
+    @trips = current_user.reservations.where("status = ?", true)
+  end
+
   def your_reservations
     @rooms = current_user.rooms
   end
-  
+
   private
   def is_conflict(start_date, end_date)
     room = Room.find(params[:room_id])
-    
+
     check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
     check.size > 0? true : false
   end
-  
+
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  devise_for  :users, 
-              :path => "", 
+  devise_for  :users,
+              :path => "",
               :path_names => {:sign_in => "login", :sign_out => "logout", :edit => "profile"},
               :controllers => {:omniauth_callbacks => "omniauth_callbacks", :registrations => 'registrations'}
 
   resources :users, only: [:show]
   resources :rooms
   resources :photos
-  
+
   resources :rooms do
     resources :reservations, only: [:create]
   end
@@ -21,8 +21,11 @@ Rails.application.routes.draw do
 
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
-  
+
   get '/your_trips' => 'reservations#your_trips'
   get '/your_reservations' => 'reservations#your_reservations'
-  
+
+  post '/notify' => 'reservations#notify'
+  post '/your_trips' => 'reservations#your_trips'
+
 end
